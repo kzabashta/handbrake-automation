93834494d7539fae1646b4e7f0872a06c1104df8
kzabashta <kosta.zabashta@gmail.com>
2018-03-24 15:35:02 -0400

    more changes

--- main.py
+++ main.py
@@ -1,35 +1,48 @@
 import os
 import mimetypes
+import subprocess
 import configparser
+
+import click
 
 def __get_source_location(config):
     return config['source']['location']
 
 def __copy_temp(config, source_file_path):
+    buffer_size=16*1024
+
     source_path = config['source']['location']
     copy_file_path = source_file_path.replace(source_path, '')[1:]
     destination_path = os.path.join(config['temp']['location'], copy_file_path)
-
-    length=16*1024
-    copied = 0
-
+    """
     if not os.path.exists(os.path.dirname(destination_path)):
          os.makedirs(os.path.dirname(destination_path))
 
+
+    try:
+        os.remove(destination_path)
+    except OSError:
+        pass
+
+
     fsource = open(source_file_path, 'rb')
     fdestination = open(destination_path, 'wb')
-    fsize = os.path.getsize(source_file_path) / 1024
+    fsize = os.path.getsize(source_file_path)
 
-    while True:
-        buf = fsource.read(length)
-        if not buf:
-            break
-        fdestination.write(buf)
-        copied += len(buf)
-        print(copied)
-    
+    bar_label = 'Copying file %s' % source_file_path
+
+    with click.progressbar(length=fsize, label=bar_label) as bar:
+        while True:
+            buf = fsource.read(buffer_size)
+            if not buf:
+                break
+            fdestination.write(buf)
+            bar.update(len(buf))
+
     fsource.close()
     fdestination.close()
+    """
+    return destination_path
 
 def scan_source(location):
     file_list = []
@@ -41,6 +54,16 @@ def scan_source(location):
                 file_list.append(file_path)
     return file_list
 
+def convert(source_path, config):
+    handbrake_cli = config['handbrake']['location']
+    source_path = __copy_temp(config, source_path)
+    filename = os.path.splitext(os.path.basename(source_path))[0]+'.mp4'
+    converted_temp_path = os.path.join(config['converted_temp']['location'], filename)
+
+    handbrake_cli += ' -i ' + source_path + ' -o ' + converted_temp_path + ' --preset="Apple 1080p30 Surround"'
+    subprocess.call(handbrake_cli)
+    print("DONE")
+
 def main():
     config = configparser.ConfigParser()
     config.read('config.ini')
@@ -50,7 +73,8 @@ def main():
 
     # start copying
     for file_path in file_list:
-        __copy_temp(config, file_path)
+        convert(file_path, config)
+        break
 
 if __name__ == '__main__':
     main()
